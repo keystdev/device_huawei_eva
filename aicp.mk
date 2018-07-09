8dd94fb04b3925d4ebf9bc0303ba3d19fea13a8e
KEASTi <31636248+keasti@users.noreply.github.com>
2018-07-09 23:33:43 +0900

    eva: AICP-ify

--- aicp.mk
+++ aicp.mk
@@ -1,18 +1,17 @@
-#
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2018 AICP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -20,8 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
 
 # Inherit from device
 $(call inherit-product, device/huawei/eva/device.mk)
@@ -30,8 +32,21 @@ $(call inherit-product, device/huawei/eva/device.mk)
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := full_eva
+PRODUCT_NAME := aicp_eva
 PRODUCT_DEVICE := eva
 PRODUCT_BRAND := Huawei
 PRODUCT_MANUFACTURER := HUAWEI
 PRODUCT_MODEL := HUAWEI-P9
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
+
+BOARD_VENDOR := huawei
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="keasti"
